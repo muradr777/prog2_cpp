60bbc73602d135cd200897534c26982ed505014c
Murad Rustamov <mrustamov@studenten.hs-bremerhaven.de>
2020-05-16 23:46:09 +0200

    Build script and .gitignore

--- First.cpp
+++ First.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int len = 14;
+    int len = 12;
     cout << len << endl;
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# compiling given file
+g++ -o "./output/$1" "$1.cpp"
+./output/$1
